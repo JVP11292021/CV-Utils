b2ac38f47feb38f6bceb56f5b6e768f476d4bb1e
Jessy van Polanen <jessyvanpolanen@gmail.com>
2021-12-09 14:57:20 +0100

    This file holds all the metadata for the project

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 NAME = "cv-utils"
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 DESCRIPTION = "Package to help developers with their opencv-python adventures."
 AUTHOR = "Jessy van Polanen"
 AUTHOR_EMAIL = "jessyvanpolanen@gmail.com"
