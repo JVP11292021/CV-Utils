e3f16a95f064345f5e01fba14f96d3e405b3c5e1
Jessy van Polanen <jessyvanpolanen@gmail.com>
2021-12-19 16:41:04 +0100

    This file makes all the imports

--- Models/__init__.py
+++ Models/__init__.py
@@ -1,3 +1,3 @@
-from Models.models import FPS, face_encodings, face_locations, for_each_img, close_win, file_splitter, draw_custom_text, draw_background, change_res, CirButton, RecButton, img_stacker, get_contours, rescale_frame, FaceDetection, FaceRecognition, HandTracker, LiveColorDetector, img_hist_bgr, img_hist_gray, rotate, translate, corner_rectangle
+from Models.models import FPS, face_encodings, face_locations, for_each_img, close_win, file_splitter, draw_custom_text, draw_background, change_res, CirButton, RecButton, img_stacker, get_contours, rescale_frame, FaceDetection, FaceRecognition, HandTracker, LiveColorDetector, img_hist_bgr, img_hist_gray, rotate, translate, corner_rectangle, MobNetObjectDetection
 import Models.constants
 from Models.constants import *
